d07aba97e6ce7552f4d97f73c0a5e144a8167cba
Tobias Boceck <boceckts@gmail.com>
2017-06-08 13:38:37 +0200

    Removed merge as this can be achieved by using pull with new commit id

--- src/main/java/com/eadlsync/model/repo/IRepo.java
+++ src/main/java/com/eadlsync/model/repo/IRepo.java
@@ -1,6 +1,6 @@
 package com.eadlsync.model.repo;
 
-import com.eadlsync.model.decision.YStatementJustificationWrapper;
+import com.eadlsync.EADLSyncExecption;
 
 /**
  * Created by tobias on 07/03/2017.
@@ -11,105 +11,34 @@ import com.eadlsync.model.decision.YStatementJustificationWrapper;
 public interface IRepo {
 
     /**
-     * Sets the context YStatementField for the decision with the given id to the given String.
-     *
-     * @param context as String
-     * @param id
-     */
-    void updateContext(String context, String id);
-
-    /**
-     * Sets the facing YStatementField for the decision with the given id to the given String.
-     *
-     * @param facing as String
-     * @param id
-     */
-    void updateFacing(String facing, String id);
-
-    /**
-     * Sets the chosen YStatementField for the decision with the given id to the given String.
-     *
-     * @param chosen as String
-     * @param id
-     */
-    void updateChosen(String chosen, String id);
-
-    /**
-     * Sets the neglected YStatementField for the decision with the given id to the given String.
-     *
-     * @param neglected as String
-     * @param id
-     */
-    void updateNeglected(String neglected, String id);
-
-    /**
-     * Sets the achieving YStatementField for the decision with the given id to the given String.
-     *
-     * @param achieving as String
-     * @param id
-     */
-    void updateAchieving(String achieving, String id);
-
-    /**
-     * Sets the accepting YStatementField for the decision with the given id to the given String.
-     *
-     * @param accepting as String
-     * @param id
-     */
-    void updateAccepting(String accepting, String id);
-
-    /**
-     * Sets the moreInformation YStatementField for the decision with the given id to the given String.
-     *
-     * @param moreInformation as String
-     * @param id
-     */
-    void updateMoreInformation(String moreInformation, String id);
-
-    /**
-     * Updates the decision with the same id as the given decision.
-     *
-     * @param decision as YStatementJustificationWrapper
-     */
-    void updateDecision(YStatementJustificationWrapper decision);
-
-    /**
      * Commits and pushes the local changes to the se-repo
      *
      * @param message for the commitToBaseRepo
      * @return the commit id
-     * @throws Exception
+     * @throws EADLSyncExecption
      */
-    String commit(String message) throws Exception;
+    String commit(String message) throws EADLSyncExecption;
 
     /**
      * Pulls the changes of the latest commit of the se-repo and applies it to the local decisions.
      *
-     * @throws Exception
+     * @throws EADLSyncExecption
      */
-    void pull() throws Exception;
-
-    /**
-     * Merge the local decisions with the decisions of the se-repo of the revision with the given commit id.
-     *
-     * @param commitId
-     * @throws Exception
-     */
-    void merge(String commitId) throws Exception;
+    void pull() throws EADLSyncExecption;
 
     /**
      * Resets the decisions to the decisions of the se-repo with the given commit id.
      *
      * @param commitId
-     * @throws Exception
+     * @throws EADLSyncExecption
      */
-    void reset(String commitId) throws Exception;
+    void reset(String commitId) throws EADLSyncExecption;
 
     /**
      * Reloads the embedded ads for this repository.
      *
-     * @throws Exception
+     * @throws EADLSyncExecption
      */
-    void reloadEADs() throws Exception;
+    void reloadEADs() throws EADLSyncExecption;
 
 }
