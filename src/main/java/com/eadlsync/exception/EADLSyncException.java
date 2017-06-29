69d7e796020f1d3f88a108df95e93132dece50ce
Tobias Boceck <boceckts@gmail.com>
2017-06-29 12:11:47 +0200

    Refactored and moved EADLSyncException

---
+++ src/main/java/com/eadlsync/exception/EADLSyncException.java
@@ -0,0 +1,30 @@
+package com.eadlsync.exception;
+
+/**
+ *
+ */
+public class EADLSyncException extends Exception {
+
+    private EADLSyncOperationState state = EADLSyncOperationState.NONE;
+
+    public EADLSyncException(EADLSyncOperationState state) {
+        this.state = state;
+    }
+
+    public enum EADLSyncOperationState {
+        PULL_FIRST,
+        CONFLICT,
+        UP_TO_DATE,
+        SYNCED,
+        NONE
+    }
+
+    public static EADLSyncException ofState(EADLSyncOperationState state) {
+        return new EADLSyncException(state);
+    }
+
+    public EADLSyncOperationState getState() {
+        return this.state;
+    }
+
+}
