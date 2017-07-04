6f65d4e48ab93d891005eb834c484a8e9c6e4a1b
Tobias Boceck <boceckts@gmail.com>
2017-07-04 19:16:04 +0200

    Reworked command test super class

--- src/test/java/com/eadlsync/cli/command/CommandTest.java
+++ src/test/java/com/eadlsync/cli/command/CommandTest.java
@@ -1,53 +1,80 @@
 package com.eadlsync.cli.command;
 
-import java.io.IOException;
-
 import com.beust.jcommander.JCommander;
+import com.eadlsync.CodeRepoMock;
 import com.eadlsync.cli.option.MainOption;
-import com.eadlsync.net.serepo.SeRepoServerTest;
+import com.eadlsync.net.serepo.SeRepoTestServer;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
+import static com.eadlsync.data.TestDataProvider.*;
 
 /**
  *
  */
-public class CommandTest extends SeRepoServerTest {
+public class CommandTest extends SeRepoTestServer {
 
-    static final InitCommand INIT_COMMAND = new InitCommand();
-    static final DeInitCommand DE_INIT_COMMAND = new DeInitCommand();
-    static final ConfigCommand CONFIG_COMMAND = new ConfigCommand();
-    static final StatusCommand STATUS_COMMAND = new StatusCommand();
-    static final CommitCommand COMMIT_COMMAND = new CommitCommand();
-    static final PullCommand PULL_COMMAND = new PullCommand();
-    static final MergeCommand MERGE_COMMAND = new MergeCommand();
-    static final ResetCommand RESET_COMMAND = new ResetCommand();
-    static final SyncCommand SYNC_COMMAND = new SyncCommand();
+    final InitCommand INIT_COMMAND = new InitCommand();
+    final DeInitCommand DE_INIT_COMMAND = new DeInitCommand();
+    final ConfigCommand CONFIG_COMMAND = new ConfigCommand();
+    final StatusCommand STATUS_COMMAND = new StatusCommand();
+    final CommitCommand COMMIT_COMMAND = new CommitCommand();
+    final PullCommand PULL_COMMAND = new PullCommand();
+    final MergeCommand MERGE_COMMAND = new MergeCommand();
+    final ResetCommand RESET_COMMAND = new ResetCommand();
+    final SyncCommand SYNC_COMMAND = new SyncCommand();
+    final MainOption option = new MainOption();
+    final JCommander commander = JCommander.newBuilder().addObject(option).
+            addCommand(InitCommand.NAME, INIT_COMMAND).
+            addCommand(DeInitCommand.NAME, DE_INIT_COMMAND).
+            addCommand(ConfigCommand.NAME, CONFIG_COMMAND).
+            addCommand(StatusCommand.NAME, STATUS_COMMAND).
+            addCommand(CommitCommand.NAME, COMMIT_COMMAND).
+            addCommand(PullCommand.NAME, PULL_COMMAND).
+            addCommand(MergeCommand.NAME, MERGE_COMMAND).
+            addCommand(ResetCommand.NAME, RESET_COMMAND).
+            addCommand(SyncCommand.NAME, SYNC_COMMAND).
+            build();
 
-    JCommander commander;
+    static final CodeRepoMock codeRepoMock = new CodeRepoMock();
+    static final SeRepoTestServer seRepoTestServer = new SeRepoTestServer();
+
+    @BeforeClass
+    public static void classSetUp() throws Exception {
+        seRepoTestServer.start();
+        codeRepoMock.createCodeRepo();
+    }
 
     @Before
-    public void initializeEadlSync() throws IOException, UnirestException {
-        MainOption option = new MainOption();
-        commander = JCommander.newBuilder().addObject(option).
-                addCommand(InitCommand.NAME, INIT_COMMAND).
-                addCommand(DeInitCommand.NAME, DE_INIT_COMMAND).
-                addCommand(ConfigCommand.NAME, CONFIG_COMMAND).
-                addCommand(StatusCommand.NAME, STATUS_COMMAND).
-                addCommand(CommitCommand.NAME, COMMIT_COMMAND).
-                addCommand(PullCommand.NAME, PULL_COMMAND).
-                addCommand(MergeCommand.NAME, MERGE_COMMAND).
-                addCommand(ResetCommand.NAME, RESET_COMMAND).
-                addCommand(SyncCommand.NAME, SYNC_COMMAND).
-                build();
+    public void methodSetUp() throws IOException, UnirestException {
+        // one commit with four decisions is always available and we have the same decisions as eadls in our code repo
+        seRepoTestServer.createRepository();
+        seRepoTestServer.createCommit(getBasicDecisionsAsSeItemsWithContent());
+
+        codeRepoMock.createClassesForEadls(getBasicDecisionsAsEadl());
+
         commander.parse(InitCommand.NAME, "-u", LOCALHOST_SEREPO, "-p", TEST_REPO);
         INIT_COMMAND.initialize();
     }
 
     @After
-    public void deInitializeEadlSync() throws IOException {
+    public void methodTearDown() throws IOException {
+        seRepoTestServer.deleteRepository();
+        codeRepoMock.cleanCodeRepo();
+
         commander.parse(DeInitCommand.NAME);
         DE_INIT_COMMAND.deInit();
+    }
+
+    @AfterClass
+    public static void classTearDown() throws Exception {
+        seRepoTestServer.stop();
+        codeRepoMock.deleteCodeRepo();
     }
 
 
